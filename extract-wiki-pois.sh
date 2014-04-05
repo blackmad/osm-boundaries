ccf0c84f16f6a22d0b88b194fd2d9c5e7f1cbd6e
David Blackman <david@blackmad.com>
2014-04-05 15:44:11 -0400

    fix  read/write arg order

--- extract-wiki-pois.sh
+++ extract-wiki-pois.sh
@@ -3,12 +3,12 @@
 set -x
 
 osmosis \
-  --write-pbf wiki_pois.osm.pbf \
+  --read-pbf $1
   --tf accept-ways wikipedia=* \
   --tf accept-relations wikipedia=* \
   --tf reject-relations boundary=administrative \
   --tf reject-ways boundary=administrative \
   --used-way \
   --used-node \
-  --read-pbf $1
+  --write-pbf wiki_pois.osm.pbf \
 
