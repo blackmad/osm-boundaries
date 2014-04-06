5c2482267e12e6173ec9d994336938b83815d471
David Blackman <david@blackmad.com>
2014-04-06 09:48:56 -0400

    fix syntax

--- extract-wiki-pois.sh
+++ extract-wiki-pois.sh
@@ -3,18 +3,18 @@
 set -x
 
 osmosis \
-  --read-pbf $1
+  --read-pbf $1 \
   --tf accept-ways wikipedia=* \
   --tf reject-ways boundary=administrative \
   --used-way \
   --used-node \
-  --write-pbf wiki_ways.osm.pbf \
+  --write-pbf wiki_ways.osm.pbf
 
 osmosis \
-  --read-pbf $1
+  --read-pbf $1 \
   --tf accept-relations wikipedia=* \
   --tf reject-relations boundary=administrative \
   --used-way \
   --used-node \
-  --write-pbf wiki_relations.osm.pbf \
+  --write-pbf wiki_relations.osm.pbf
 
