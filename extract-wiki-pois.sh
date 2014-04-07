0b1280c76d63fad5c056e3b856774b7b5ff3cf9c
David Blackman <david@blackmad.com>
2014-04-07 11:44:54 -0400

    a few more poi filters

--- extract-wiki-pois.sh
+++ extract-wiki-pois.sh
@@ -6,6 +6,10 @@ osmosis \
   --read-pbf $1 \
   --tf accept-ways wikipedia=* \
   --tf reject-ways boundary=administrative \
+  --tf reject-ways boundary=census \
+  --tf reject-ways natural=glacier \
+  --tf reject-ways natural=massif \
+  --tf reject-ways place=locality \
   --tf reject-relations \
   --used-node \
   --write-pbf wiki_ways.osm.pbf
