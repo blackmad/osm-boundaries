df171a4d5a95ab564c603b120d7a0cbd784b76b1
David Blackman <david@blackmad.com>
2014-04-07 09:17:52 -0400

    try again to extract wiki-ways

--- extract-wiki-pois.sh
+++ extract-wiki-pois.sh
@@ -6,7 +6,7 @@ osmosis \
   --read-pbf $1 \
   --tf accept-ways wikipedia=* \
   --tf reject-ways boundary=administrative \
-  --used-way \
+  --tf reject-relations \
   --used-node \
   --write-pbf wiki_ways.osm.pbf
 
@@ -14,6 +14,7 @@ osmosis \
   --read-pbf $1 \
   --tf accept-relations wikipedia=* \
   --tf reject-relations boundary=administrative \
+  --tf reject-relations natural=glacier \ 
   --used-way \
   --used-node \
   --write-pbf wiki_relations.osm.pbf
